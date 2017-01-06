78ab50af2c971f850beae7f5342aee4fcc432a0c
Dave Lively <davelively14@gmail.com>
2017-01-06 14:31:07 -0500

    Completed

--- hamming/hamming.rb
+++ hamming/hamming.rb
@@ -1,1 +1,20 @@
 # Something
+class Hamming
+  def Hamming.compute(str1, str2)
+    ans = 0
+    i = 0
+
+    if str1.length != str2.length
+      raise ArgumentError
+    end
+
+    while i < str1.length do
+      if str1[i] != str2[i]
+        ans = ans + 1
+      end
+      i = i + 1
+    end
+
+    return ans
+  end
+end
