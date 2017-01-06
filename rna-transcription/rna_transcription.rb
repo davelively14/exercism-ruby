a0dd434556b3a93abde76a85992e8082a124b00b
Dave Lively <davelively14@gmail.com>
2017-01-06 18:15:00 -0500

    Completed

--- rna-transcription/rna_transcription.rb
+++ rna-transcription/rna_transcription.rb
@@ -1,3 +1,25 @@
 module BookKeeping
   VERSION = 4
 end
+module Complement
+  def Complement.of_dna(str)
+    ans = ""
+    max_index = str.length - 1
+    (0..max_index).each do |i|
+      case str[i]
+      when "G"
+        ans = ans + "C"
+      when "C"
+        ans = ans + "G"
+      when "T"
+        ans = ans + "A"
+      when "A"
+        ans = ans + "U"
+      else
+        return ""
+      end
+    end
+
+    return ans
+  end
+end
