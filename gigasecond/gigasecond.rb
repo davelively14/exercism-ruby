54605b5a1871b0cfaf62eee71b2b068c168f3615
Dave Lively <davelively14@gmail.com>
2017-01-06 17:58:33 -0500

    Completed

--- gigasecond/gigasecond.rb
+++ gigasecond/gigasecond.rb
@@ -1,1 +1,22 @@
-# More
+module BookKeeping
+  VERSION = 5
+end
+class Gigasecond
+  def Gigasecond.from(time)
+    time + 1_000_000_000
+  end
+
+  # Unnecessary, but I like it so keeping it here
+  def Gigasecond.leap_year?(year)
+    case
+    when year % 400 == 0
+      true
+    when year % 100 == 0
+      false
+    when year % 4 == 0
+      true
+    else
+      false
+    end
+  end
+end
