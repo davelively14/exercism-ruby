2c0f331b810ff894be176fec2ed6f5655d81ce61
Dave Lively <davelively14@gmail.com>
2017-01-06 21:53:44 -0500

    Completed

--- raindrops/raindrops.rb
+++ raindrops/raindrops.rb
@@ -1,3 +1,22 @@
 module BookKeeping
   VERSION = 3
 end
+class Raindrops
+  FACTORS = [[3, "Pling"], [5, "Plang"], [7, "Plong"]]
+
+  def Raindrops.convert(number)
+    result = ""
+
+    FACTORS.each do |x|
+      if number % x[0] == 0
+        result += x[1]
+      end
+    end
+
+    if result == ""
+      return "#{number}"
+    else
+      return result
+    end
+  end
+end
