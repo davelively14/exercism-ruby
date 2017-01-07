5fc4873098ae99bb7b87afc082bccd8f29e6cfcc
Dave Lively <davelively14@gmail.com>
2017-01-07 08:28:14 -0500

    Completed

--- difference-of-squares/difference_of_squares.rb
+++ difference-of-squares/difference_of_squares.rb
@@ -3,15 +3,18 @@ module BookKeeping
 end
 class Squares
   def initialize(num)
-    1..num
+    @series = 0..num
   end
 
-  def square_of_sum(series)
-    total = series.reduce(:+)
-    return total*total
+  def square_of_sum
+    return @series.reduce(:+)**2
   end
 
   def sum_of_squares
+    return @series.inject { |sum, n| sum + n**2 }
+  end
 
+  def difference
+    return square_of_sum - sum_of_squares
   end
 end
