9183e4e2bdcd3b537405dbc37dc53268a1aa9fbf
Dave Lively <davelively14@gmail.com>
2017-01-08 08:35:48 -0500

    Completed

--- pangram/pangram.rb
+++ pangram/pangram.rb
@@ -3,12 +3,8 @@ module BookKeeping
 end
 class Pangram
   def Pangram.pangram?(str)
-    ("a".."z").each do |c|
-      if str.downcase.scan(c).count == 0
-        return "#{str.inspect} is NOT a pangram"
-      end
+    ("a".."z").all? do |c|
+      str.downcase.include? c
     end
-
-    return "#{str.inspect} IS a pangram"
   end
 end
